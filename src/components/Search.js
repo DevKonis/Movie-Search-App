8f287f160efe41c0f27f4d14dc3742fdf01af223
DevKonis <118440358+DevKonis@users.noreply.github.com>
2023-08-14 13:53:13 +0900

    update 8 files

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,29 +1,27 @@
 import { Component } from "../core/core";
-import movieStore, { searchMovies } from "../store/movie"
+import movieStore, { searchMovies } from "../store/movie";
 
 export default class Search extends Component {
-  render () {
-    this.el.classList.add('search')
-    this.el.innerHTML = /* HTML */`
-      <input placeholder="Enter the moive title to search!"/>
-      <button class="btn btn-primary">
-        Search!
-      </button>
-    `
-    const inputEl = this.el.querySelector('input')
-    inputEl.addEventListener('input', () => {
-      movieStore.state.searchText = inputEl.value
-    })
-    inputEl.addEventListener('keydown', event => {
-      if (event.key === 'Enter' && movieStore.state.searchText.trim()) {
-        searchMovies(1)
+  render() {
+    this.el.classList.add("search");
+    this.el.innerHTML = /* HTML */ `
+      <input placeholder="Enter the moive title to search!" />
+      <button class="btn btn-primary">Search!</button>
+    `;
+    const inputEl = this.el.querySelector("input");
+    inputEl.addEventListener("input", () => {
+      movieStore.state.searchText = inputEl.value;
+    });
+    inputEl.addEventListener("keydown", (event) => {
+      if (event.key === "Enter" && movieStore.state.searchText.trim()) {
+        searchMovies(1);
       }
-    })
-    const btnEl = this.el.querySelector('.btn')
-    btnEl.addEventListener('click', () => {
+    });
+    const btnEl = this.el.querySelector(".btn");
+    btnEl.addEventListener("click", () => {
       if (movieStore.state.searchText.trim()) {
-        searchMovies(1)
+        searchMovies(1);
       }
-    })
+    });
   }
 }
---
+++ src/routes/Movie.js
@@ -0,0 +1,11 @@
+import { Component } from "../core/core";
+import movieStore, { getMovieDetails } from '../store/movie'
+
+export default class Movie extends Component {
+  async render() {
+    await getMovieDetails(history.state.id)
+    console.log(movieStore.state.movie)
+
+    this.el.classList.add('container', 'the-movie')
+  }
+}
