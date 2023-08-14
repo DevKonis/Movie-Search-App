84ee116bd3fc137281626321b14e0fd40963e3d4
DevKonis <118440358+DevKonis@users.noreply.github.com>
2023-08-14 16:06:00 +0900

    update 9 files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { Component } from './core/core'
+import { Component } from "./core/core";
+import TheHeader from "./components/TheHeader";
 
 export default class App extends Component {
   render() {
-    const routerView = document.createElement('router-view')
-    this.el.append(routerView)
+    const theHeader = new TheHeader().el;
+    const routerView = document.createElement("router-view");
+
+    this.el.append(theHeader, routerView);
   }
 }
